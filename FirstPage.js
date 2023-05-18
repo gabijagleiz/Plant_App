b7cf767c82b5262392589053cde9afe25d008e8c
gabijagleiz <79048184+gabijagleiz@users.noreply.github.com>
2023-05-18 14:22:54 +0300

    Update FirstPage.js

--- FirstPage.js
+++ FirstPage.js
@@ -1,23 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, Image } from 'react-native';
+import plantData from './plantsData/';
 
-export default function FirstPage() {
+const FirstPage = () => {
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [selectedPlant, setSelectedPlant] = React.useState(null);
+
+  const handleImagePress = (plant) => {
+    setModalVisible(!modalVisible);
+    setSelectedPlant(plant)
+  };
+
+  const styles = {
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 20,
+    },
+
+    row: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+    },
+
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    lipstickPlantContainer: {
+      position: 'absolute',
+      top: 20,
+      left: 20,
+      width: '45%',
+      zIndex: 1,
+    },
+    lipstickPlant: {
+      width: 150,
+      height: 150,
+    },
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>First page</Text>
-      <StatusBar style="auto" />
+    <View style={styles.row}>
+      {plantData.map(plant => 
+        <View style={styles.container} key={plant.id}>
+          <TouchableOpacity onPress={() => handleImagePress(plant)} style={styles.lipstickPlantContainer}>
+            <Image source={plant.image} style={styles.lipstickPlant}/>
+            <Text>{plant.name}</Text>
+            
+            {/* <Image  source={require('./assets/lipstickplant.jpg')} style={styles.lipstickPlant} /> */}
+          </TouchableOpacity>
+
+          <Modal
+            visible={modalVisible}
+            animationType="slide"
+            transparent={true}
+            onRequestClose={() => setModalVisible(false)}
+          >
+            <View style={styles.modalContainer}>
+              {selectedPlant && (
+                <View>
+    
+                  <Text>{selectedPlant.name}</Text>
+                  <Text>{selectedPlant.description}</Text>
+                  <Text>{selectedPlant.wateringInstructions}</Text>
+                  <Text>{selectedPlant.lightRequirements}</Text>
+                {/* Add more content here */}
+              </View>)}
+            </View>
+          </Modal>
+        </View>)}
     </View>
   );
+};
 
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-});
+export default FirstPage;
