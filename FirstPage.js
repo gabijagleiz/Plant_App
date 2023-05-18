70dd93f6d3a7b4f056907e9996b39bc552cdcd0b
gabijagleiz <79048184+gabijagleiz@users.noreply.github.com>
2023-05-18 20:45:03 +0300

    Update FirstPage.js

--- FirstPage.js
+++ FirstPage.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import {ScrollView, View, Text, TouchableOpacity, Modal, Image, Dimensions } from 'react-native';
+import React, { useEffect,useContext  } from 'react';
+import {ScrollView, View, Text, TouchableOpacity, Modal, Image, Dimensions,Pressable,Alert } from 'react-native';
 import plantData from './plantsData/';
+import { useNavigation,useIsFocused  } from '@react-navigation/native';
+import { PlantContext } from './plantContext';
 
 const FirstPage = () => {
   const [modalVisible, setModalVisible] = React.useState(false);
   const [selectedPlant, setSelectedPlant] = React.useState(null);
 
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  const { addPlant } = useContext(PlantContext);
+
   const handleImagePress = (plant) => {
     setModalVisible(!modalVisible);
     setSelectedPlant(plant);
+  };
+
+  const handleAddPlant = () => {
+    if (selectedPlant) {
+      addPlant(selectedPlant); // Add the selected plant to the context  
+      Alert.alert('Augalas pridėtas');
+      setModalVisible(false);
+    }
   };
 
   const screenWidth = Dimensions.get('window').width;
@@ -34,8 +48,8 @@ const FirstPage = () => {
     },
 
     lipstickPlant: {
-      width: 150,
-      height: 150,
+      width: 200,
+      height: 200,
     },
     modalContainer: {
       flex: 1,
@@ -60,10 +74,39 @@ const FirstPage = () => {
       fontSize: 15,
       color: '#F5F5F5',
     },
+
+    button: {
+      width: "35%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#c58d6b",
+    },
+
+    btntext:{
+      color: '#ffffff',
+      fontSize: 15,
+    },
+
+    space: {
+      width: 20, // or whatever size you need
+      height: 20,
+    },
   };
+
+  useEffect(() => {
+    if (!isFocused) {
+      setModalVisible(false); // Reset modalVisible state when component is not focused
+    }
+  }, [isFocused]);
 
   return (
     <ScrollView>
+      <Pressable style={styles.button} onPress={() => navigation.navigate('MyPlants')}>
+          <Text style= {styles.btntext}>Mano augalai</Text>
+      </Pressable>
       <View style={styles.row}>
         {plantData.map((plant) => (
           <View style={styles.container} key={plant.id}>
@@ -88,6 +131,15 @@ const FirstPage = () => {
                     {/* Add more content here */}
                   </View>
                 )}
+                <View style={styles.row}>
+                  <Pressable style={styles.button} onPress={() => setModalVisible(false)}>
+                    <Text style= {styles.btntext}>Atšaukti</Text>
+                  </Pressable>
+                  <View style={styles.space} />
+                  <Pressable style={styles.button} onPress={handleAddPlant}>
+                    <Text style= {styles.btntext}>Pridėti augalą</Text>
+                  </Pressable>
+                </View>
               </View>
             </Modal>
           </View>
